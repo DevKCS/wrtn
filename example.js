e819041523dc4d3f67884f9c9b8273cc5ed6b136
KimChanSol <kim3834000p@gmail.com>
2023-06-03 00:29:20 +0900

    Update example.js
    Signed-off-by: KimChanSol <kim3834000p@gmail.com>

--- example.js
+++ example.js
@@ -1,13 +1,14 @@
 import {addAccount, wrtn} from './wrtn.js';
-console.log(await addAccount("asdfgh@test.com","w"))
+//console.log(await addAccount("asdfgh@test.com","w"))
 
 
 let t = new wrtn();
-await t.loginByEmail("wrtntest02@ruu.kr","w")
+await t.loginByEmail("restrent@gmail.com","test1234!")
 console.time()
 console.log(t.loginToken)
-console.log(await t.roomList())
-console.log(await t.refresh())
-//console.log(await t.removeRoom('645b69e6ac512fd38c1c5288'))
-console.log(await t.roomList())
+console.log(await t.ask("안녕?","GPT4.0","646dec85cd7daeafede40b1e"))
+let fileId = await t.upload(readFileSync("333.pdf").buffer)
+console.log("fileId : "+fileId)
+console.log(await t.ask("이 파일의 주요내용을 요약해","GPT3.5","646dec85cd7daeafede40b1e",fileId))
+console.log(await t.askWithSearch("오늘 날짜","646dec85cd7daeafede40b1e"))
 console.timeEnd()
